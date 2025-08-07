8d374fd9d59b2da73bb9166043089e0389817b4d
caca-pipi-1 <caca.pipi.4@hotmail.com>
2025-08-07 11:08:38 +0200

    Add france 2

---
+++ auth_links/france2.py
@@ -0,0 +1,13 @@
+import requests
+import streamlink
+import urllib.parse
+
+manifest = requests.post("https://api.ssai.ftven.fr/v1/session/c6f323e65336fbba9ea766d582216fd61ed74452/SSAIFrance2OTTEMTConfiguration/out/v1/3f5d86a548454a27ab9a06078aa68a03/index.m3u8").json()["manifestUrl"]
+
+url = "https://live-ssai-v2.ftven.fr/dai" + manifest
+
+m3u8 = requests.get("https://hdfauth.ftven.fr/esi/TA?format=json&url=" + urllib.parse.quote(url, safe="")).json()["url"]
+
+stream = streamlink.streams(m3u8)["best"].url
+
+print(stream)
--- auth_links/france3alsace.py
+++ auth_links/france3alsace.py
@@ -1,5 +1,8 @@
 import requests
+import streamlink
 
-stream = requests.get("https://hdfauth.ftven.fr/esi/TA?format=json&url=https%3A%2F%2Fsimulcast-fr3regions-p.ftven.fr%2Fsimulcast%2Falsace%2Fhls_francedomtom_alsace%2Fmaster.m3u8").json()["url"]
+m3u8 = requests.get("https://hdfauth.ftven.fr/esi/TA?format=json&url=https%3A%2F%2Fsimulcast-fr3regions-p.ftven.fr%2Fsimulcast%2Falsace%2Fhls_francedomtom_alsace%2Fmaster.m3u8").json()["url"]
+
+stream = streamlink.streams(m3u8)["best"].url
 
 print(stream)
